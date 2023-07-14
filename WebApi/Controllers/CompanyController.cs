dc2a75eb8ce59b572e9607862d5cb143b3a5a465
Hamed Jelodar <hajelovdar@yahoo.com>
2023-07-13 20:38:18 -0400

    Add files via upload

---
+++ WebApi/Controllers/CompanyController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using AutoMapper;
+using BusinessLayer.Model.Interfaces;
+using WebApi.Models;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WebApi.Controllers
+{
+    public class CompanyController : ApiController
+    {
+        private readonly ICompanyService _companyService;
+        private readonly IMapper _mapper;
+
+        public  CompanyController(ICompanyService companyService, IMapper mapper)
+        {
+            _companyService = companyService;
+            _mapper = mapper;
+        }
+        
+        
+        // GET api/<controller>
+        public async Task <IEnumerable<CompanyDto>> GetAll()
+        {
+            var items = _companyService.GetAllCompanies();
+            return await Task.Run(() => _mapper.Map<IEnumerable<CompanyDto>>(items));
+        }
+
+        
+        // GET api/<controller>/5
+        public async Task <CompanyDto> Get(string companyCode)
+        {
+            var item = _companyService.GetCompanyByCode(companyCode);
+            return await Task.Run(()=> _mapper.Map<CompanyDto>(item));
+        }
+
+
+
+        // POST api/<controller>
+        public async Task Post([FromBody] string value)
+        {
+
+            await Task.Run
+            (() =>
+            {
+                
+            });
+
+        }
+
+        // PUT api/<controller>/5
+        public async Task Put(int id, [FromBody]string value)
+        {
+            await Task.Run
+            (() =>
+            {
+
+            });
+
+        }
+
+
+        // DELETE api/<controller>/5
+        public async Task Delete(int id)
+        {
+            await Task.Run
+          (() =>
+          {
+
+          });
+
+        }
+    }
+}
---
+++ WebApi/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace WebApi
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+
+
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            Exception exc = Server.GetLastError();
+
+            if (exc is HttpUnhandledException)
+            {
+                // Pass the error on to the error page.
+                Server.Transfer("ErrorPage.html?handler=Application_Error%20-%20Global.asax", true);
+            }
+        }
+
+
+
+    }
+}
